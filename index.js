8844d995b21618b77ccba5d458ca2a4f3123bf8e
Eli Saado <eli5saado@gmail.com>
2019-10-07 15:53:22 +0200

    Add initializers for modules

---
+++ index.js
@@ -0,0 +1,14 @@
+// By Eli Saado and contributors
+
+// config
+const config = require('./config.json');
+
+// database
+const db = require('./db.js');
+
+// bot
+const bot = require('./bot.js');
+
+// plguins
+const Greetings = require("./plugins/greetings.js");
+const greetings = new Greetings();
