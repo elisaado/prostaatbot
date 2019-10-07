c960c1d40e030ac60cab106a2c1489d9c27248ab
Eli Saado <eli5saado@gmail.com>
2019-10-07 16:06:33 +0200

    Add logger

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 // By Eli Saado and contributors
 
+// initialize debug logger
+const log = require("debug")("prostaatbot:index");
+
+log("Starting the engines");
+
 // config
+log("Initializing config...");
 const config = require('./config.json');
+log("Config initialized!");
 
 // database
+log("Initializing database...");
 const db = require('./db.js');
+log("Database initialized!")
 
 // bot
+log("Initializing bot API...")
 const bot = require('./bot.js');
+log("Bot API initialized!");
 
 // plguins
+log("Initializing plugins...")
 const Greetings = require("./plugins/greetings.js");
 const greetings = new Greetings();
+
+log("Plugins initialized!");
+
+log("Prostaatbot to your service!");
