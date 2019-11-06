78b135b76252f1b0804c0537fcbffb44fb96184f
Eli Saado <eli5saado@gmail.com>
2019-11-06 22:02:32 +0100

    linter

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ log("Starting the engines");
 
 // config
 log("Initializing config...");
-const config = require('./config.json');
+const config = require("./config.json");
 log("Config initialized!");
 
 // database
 log("Initializing database...");
-const db = require('./db.js');
-log("Database initialized!")
+const db = require("./db.js");
+log("Database initialized!");
 
 // bot
-log("Initializing bot API...")
-const bot = require('./bot.js');
+log("Initializing bot API...");
+const bot = require("./bot.js");
 log("Bot API initialized!");
 
 // plguins
-log("Initializing plugins...")
+log("Initializing plugins...");
 const Greetings = require("./plugins/greetings.js");
 const greetings = new Greetings();
 
--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,45 +1,39 @@
-const Plugin = require('../plugin.js');
+const Plugin = require("../plugin.js");
 
 class Greetings extends Plugin {
-    constructor(bot) {
-        super("Greetings", bot)
+  constructor(bot) {
+    super("Greetings", bot);
 
-        // initialize "say welcome"
-        this.sayWelcome();
+    // initialize "say welcome"
+    this.sayWelcome();
 
-        // initialize "say bye"
-        this.sayBye();
-    }
+    // initialize "say bye"
+    this.sayBye();
+  }
 
-    // Says welcome to people :)
-    sayWelcome() {
-        this.bot.on('message', msg => {
-            console.log(this.bot)
+  // Says welcome to people whenever they join
+  sayWelcome() {
+    this.bot.on("message", msg => {
+      if (msg.new_chat_members == null || msg.new_chat_members.length == 0)
+        return;
 
-            console.log(msg);
+      let members = msg.new_chat_members;
+      for (let member of members) {
+        this.bot.sendMessage(msg.chat.id, `Welcome ${member.first_name}!`);
+      }
+    });
+  }
 
-            if (msg.new_chat_members == null
-                || msg.new_chat_members.length == 0) return;
-            
-            let members = msg.new_chat_members;
-            for (let member of members) {
-                this.bot.sendMessage(msg.chat.id, `Welcome ${member.first_name}!`)                
-            }
+  // Says bye when they leave
+  sayBye() {
+    this.bot.on("message", msg => {
+      console.log(msg)
+      if (msg.left_chat_member == null) return;
 
-        });
-    }
-
-    // Says bye when they leave
-    sayBye() {
-        this.bot.on('message', msg => {
-            console.log(msg);
-
-            if (msg.left_chat_member == null) return;
-            
-            const member = msg.left_chat_member;
-            this.bot.sendMessage(msg.chat.id, `Bye ${member.first_name}!`)                
-        });
-    }
+      const member = msg.left_chat_member;
+      this.bot.sendMessage(msg.chat.id, `Bye ${member.first_name}!`);
+    });
+  }
 }
 
 module.exports = Greetings;
